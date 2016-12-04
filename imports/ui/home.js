9173ff1964635bc1991865e1a150dc2b31d8f116
GwFreak01 <gwfreak01@gmail.com>
2016-12-04 15:49:48 -0500

    Side Bar Fixed Mostly           Need to Change links
    Navbar in progress
             User Account needs to be authenticated

--- imports/ui/home.js
+++ imports/ui/home.js
@@ -3,6 +3,25 @@
  */
 import {Template} from 'meteor/templating';
 
-// import '../../public/images/';
+// import '../../public/images/EagerLogo.png';
 import './home.html';
 import './home.css';
+
+Template.home.helpers({
+    dateVar: function () {
+        var weekDay = ["Sun.", "Mon.", "Tue.", "Wed.", "Thu.", "Fri.", "Sat."];
+        var monthNames = ["Jan.", "Feb.", "Mar.", "Apr.", "May", "Jun.",
+            "Jul.", "Aug.", "Sep.", "Oct.", "Nov.", "Dec."];
+        var today = new Date();
+        var day = weekDay[today.getDay()];
+        var month = monthNames[today.getMonth()];
+
+        var date = day + " " + month + " " + today.getDate() + ", " + today.getFullYear();
+        return date;
+    },
+
+    studentName: function () {
+
+    }
+
+});
